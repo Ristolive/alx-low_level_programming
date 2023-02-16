cb63cb7b2398e8d431f6d7998f5c4f039d7ba730
Ristolive <ristolive@gmail.com>
2023-02-16 08:09:54 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: 0 after printing the function
+ */
+
+int main
+{
+	puts("\"with proper grammer, but the outcome is a piece of art");
+	return (0);
+}
