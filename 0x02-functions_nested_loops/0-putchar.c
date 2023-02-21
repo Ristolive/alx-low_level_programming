1d58f2ff582fabe8e12be7f8084f18e73cb021d3
Ristolive <ristolive@gmail.com>
2023-02-21 14:50:15 -0800

    push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,21 @@
 
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
+    int _putchar(int c);
+    char str[] = "_putchar";
+    int ch;
 
-	for (ch = 0; ch < 8; ++ch)
-	_putchar(str[ch]);
-	_putchar('\n');
+    for (ch = 0; ch < 8; ++ch) {
+        _putchar(str[ch]);
+    }
 
-	return (0);
+    _putchar('\n');
+
+    return (0);
 }
+
+int _putchar(int c)
+{
+    return putchar(c);
+}
+
