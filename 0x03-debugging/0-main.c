ec8f2a173990d596d3a2760f1dbb1aba3d795c76
Ristolive <ristolive@gmail.com>
2023-02-22 08:10:27 -0800

    push

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,33 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * main - tests function that prints if integar is positive or negative
- * 
+ * positive_or_negative - prints if integer is positive or negative
+ * @i: the integer to check
+ *
+ * Return: void
+ */
+void positive_or_negative(int i)
+{
+    if (i > 0)
+        printf("%d is positive\n", i);
+    else if (i < 0)
+        printf("%d is negative\n", i);
+    else
+        printf("%d is zero\n", i);
+}
+
+/**
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
-	int i;
+    int i;
 
-	i = 98 % 2;
-	positive_or_negative(i);
+    i = 98;
+    positive_or_negative(i);
 
-	return (0);
-
+    return (0);
 }
+
