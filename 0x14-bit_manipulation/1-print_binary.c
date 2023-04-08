23c467800d66c85a1acc3a55d555b411545d1624
Ristolive <ristolive@gmail.com>
2023-04-08 04:14:15 -0700

    push

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,7 @@
 #include "main.h"
-
 /**
- * print_binary - prints the binary representation
- * of a number.
- * @n: unsigned long int.
- *
- * Return: no return.
+ * print_binary - function that prints the binary representation of a number
+ * @n: number
  */
 void print_binary(unsigned long int n)
 {
