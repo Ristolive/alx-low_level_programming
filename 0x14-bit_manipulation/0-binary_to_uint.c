a87101b4fbb63056faaabafb5197faa7850d5428
Ristolive <ristolive@gmail.com>
2023-04-08 03:33:03 -0700

    push now

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int n;
-	unsigned int c;
+	unsigned int ui;
+	int len, base_two;
 
-	c = 0;
 	if (!b)
 		return (0);
-	for (n = 0; b[n] != '\0'; n++)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[n] != '0' && b[n] != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	for (n = 0; b[n] != '\0'; n++)
-	{
-		c <<= 1;
-		if (b[n] == '1')
-			c += 1;
-	}
-	return (c);
+
+	return (ui);
 }
