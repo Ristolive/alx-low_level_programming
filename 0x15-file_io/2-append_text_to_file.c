143c2cfc4b73eb395c20047d83f7df70ba2483ae
Ristolive <ristolive@gmail.com>
2023-04-10 07:06:02 -0700

    push

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,32 +6,38 @@
  * @text_content: content to append at end of file
  * Return: 1 on Success, -1 on failure
  */
+
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends a text at the end of a file.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int rd;
-	int letters;
-	int rwr;
+	int fd;
+	ssize_t len;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	rd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (rd == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (letters = 0; text_content[letters]; letters++)
-		;
-
-	rwr = write(rd, text_content, letters);
-
-	if (rwr == -1)
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (len == -1)
 		return (-1);
-
-	close(rd);
-
 	return (1);
 }
+
